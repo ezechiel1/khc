5f309be9a5da0426ebef29a032a6a52777275086
root <root@localhost.localdomain>
2019-10-04 16:05:30 +0200

    front end of the khc program

---
+++ header.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>KIGALI HOUSE CONNECT - Page Template</title>
+<meta charset="UTF-8">
+<meta name="description" content="KIGALI Landing Page Template">
+<meta name="keywords" content="KIGALI, unica, creative, html">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="icon" type="img/png" href="img/icons/favicon.ico"/>
+<!-- Favicon -->   
+<link href="img/favicon.ico" rel="shortcut icon"/>
+
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+
+<!-- Stylesheets -->
+<link rel="stylesheet" href="css/bootstrap.min.css"/>
+<link rel="stylesheet" href="css/font-awesome.min.css"/>
+<link rel="stylesheet" href="css/animate.css"/>
+<link rel="stylesheet" href="css/owl.carousel.css"/>
+<link rel="stylesheet" href="css/style.css"/>
+
+
+</head>
+<body>
+<!-- Page Preloder -->
+<div id="preloder">
+<div class="loader"></div>
+</div>
+
+<!-- Header section -->
+<header class="header-section">
+<div class="header-top">
+<div class="container">
+<div class="row">
+<div class="col-lg-6 header-top-left">
+<div class="top-info">
+<i class="fa fa-phone"></i>
+(+250) 784 077 896
+</div>
+<div class="top-info">
+<i class="fa fa-envelope"></i>
+info.kigalihouses@gmail.com
+</div>
+</div>
+
+<div class="col-lg-6 text-lg-right header-top-right">
+<div class="top-social">
+<a href=""><i class="fa fa-facebook"></i></a>
+<a href=""><i class="fa fa-twitter"></i></a>
+<a href=""><i class="fa fa-instagram"></i></a>
+<a href=""><i class="fa fa-pinterest"></i></a>
+<a href=""><i class="fa fa-linkedin"></i></a>
+</div>
+<div class="user-panel">
+<a href="Login/register.html"><i class="fa fa-user-circle-o"></i> Register</a>
+<a href="Login/indexlogin.html"><i class="fa fa-sign-in"></i> Login</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="container">
+<div class="row">
+<div class="col-12">
+<div class="site-navbar">
+<div class="nav-switch">
+<i class="fa fa-bars"></i>
+</div>
+<ul class="main-menu">
+<li><a href="index.php"><b>Home</b></a></li>
+<li><a href="categories.php"><b>FEATURED LISTING</b></a></li>
+<li><a href="about.php"><b>ABOUT US</b></a></li>
+<li><a href="single-list.php"><b>Pages</b></a></li>
+<li><a href="contact.php"><b>Contact</b></a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</header>
+<!-- Header section end -->
